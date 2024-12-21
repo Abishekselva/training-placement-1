ad6281e59a57199f653f85200fd93f8a1713120a
Abishekselva <151352160+Abishekselva@users.noreply.github.com>
2024-12-21 20:50:45 +0530

    Create Loop.java

---
+++ 21.12.2024/Loop.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for(int i=0; i<N; i++) {
+            System.out.println(i + " x " + N + " = " + (i*N));
+        }
+
+        scanner.close();
+    }
+}
