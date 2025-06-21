541a733d2de268b23c7d4ed24f4941d5e02377f9
Abishekselva <151352160+Abishekselva@users.noreply.github.com>
2025-06-21 19:09:52 +0530

    Create add numwithdiffdatatype.java

---
+++ 21.06.2025/add numbers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class user{
+  public static void main(String args[]){
+    Scanner x = new Scanner(System.in);
+    int b = x.nextInt();
+    float a = x.nextFloat();
+    System.out.println(b+a);
+  }
+}
+      
