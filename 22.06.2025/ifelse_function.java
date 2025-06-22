f9d11b0e78a140611ae88acc28604c008ade9839
Abishekselva <151352160+Abishekselva@users.noreply.github.com>
2025-06-22 19:01:54 +0530

    Create ifelse_function.java

---
+++ 22.06.2025/ifelse_function.java
@@ -0,0 +1,14 @@
+import java.lang.System;
+import java.util.Scanner;
+class user{
+  public static void main(String args[]){
+    Scanner x = new Scanner(System.in);
+    String a = x.nextLine();
+    if(a== "win"){
+      System.out.println("Esala cup namadhey");
+    }
+    else{
+      System.out.println("RCB will gives comeback");
+    }
+  }
+}
